359b17f55b6ab65cc10a66288dc8cf8582bec14b
joseph.wilson8-nhs <joseph.wilson8@nhs.net>
2024-12-12 16:04:12 +0000

    Enhances read_practice_level_crosstab_files function to accept a filename prefix and adds error handling for missing files

--- code_your_own_pandas_pipeline/data_in.py
+++ code_your_own_pandas_pipeline/data_in.py
@@ -11,8 +11,8 @@ read_practice_level_crosstab_files(directory: Optional[Path] = None) -> list[pd.
 
 from pathlib import Path
 from typing import Optional
-import pandas as pd
 
+import pandas as pd
 from loguru import logger
 from tqdm import tqdm
 
@@ -40,24 +40,38 @@ def read_mapping_data(file_path: Optional[Path] = None) -> pd.DataFrame:
     return pd.read_csv(file_path)
 
 
-def read_practice_level_crosstab_files(directory: Optional[Path] = None) -> list[pd.DataFrame]:
+def read_practice_level_crosstab_files(
+    directory: Optional[Path] = None, file_starts_with: Optional[str] = "Practice_Level_Crosstab"
+) -> list[pd.DataFrame]:
     """
-    Reads all files in the specified directory that start with 'Practice_level_crosstab'
+    Reads all files in the specified directory that start with the given prefix
     and returns them as a list of pandas DataFrames.
+
     Parameters
     ----------
     directory : Optional[Path], optional
         The directory containing the files. If not provided, the default path "RAW_DATA_DIR" will be used.
+    file_starts_with : Optional[str], optional
+        The prefix that the filenames should start with. Default is "Practice_Level_Crosstab".
     Returns
     -------
     list[pd.DataFrame]
         A list of DataFrames, each containing the data from one of the files.
+    Raises
+    ------
+    AssertionError
+        If no files are found in the specified directory with the given prefix.
+
     """
 
     if directory is None:
         directory = RAW_DATA_DIR
 
-    file_paths = list(directory.glob("Practice_level_crosstab*"))
+    file_paths = list(directory.glob(f"{file_starts_with}*.csv"))
+    assert (
+        file_paths
+    ), f"No files found in {directory} with name starting with '{file_starts_with}'"
+
     dataframes = []
     for file_path in tqdm(file_paths, desc="Reading Practice Level Crosstab Files"):
         logger.info(f"Reading file {file_path}")
